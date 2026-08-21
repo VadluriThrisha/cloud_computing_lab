2cfa694d29694999c53695c1525818615f545173
VadluriThrisha <vadlurithrisha@gmail.com>
2026-08-21 15:42:16 +0530

    Enhance home route with HTML content
    Added HTML structure to the home route response.

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    <html>
+    <body>
+    <h1>Hello Rgukt</h1>
+    </body>
+    </html>
     return "Flask application running successfully"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
+
